2af11b982464f112b00e133c48cc39fd5a6ec23f
Krystian Klimczak <klimczak777@icloud.com>
2026-07-02 07:02:19 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-#  write your code here
+from app.animals.animal import Animal
+from app.animals.cat import Cat
+from app.animals.dog import Dog
+
+
+def feed_animals(animals: list[Animal]) -> int:
+    return sum(animal.feed() for animal in animals)
