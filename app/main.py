4b183e5dedade36db94f7110f782455e7586856f
Krystian Klimczak <klimczak777@icloud.com>
2026-07-02 07:04:57 +0200

    Solution 2

--- app/main.py
+++ app/main.py
@@ -5,3 +5,9 @@ from app.animals.dog import Dog
 
 def feed_animals(animals: list[Animal]) -> int:
     return sum(animal.feed() for animal in animals)
+
+cat = Cat("Cat", False)
+lion = Animal("Lion", 25, True)
+dog = Dog("Dog")
+
+print(feed_animals([cat, lion, dog]) )
